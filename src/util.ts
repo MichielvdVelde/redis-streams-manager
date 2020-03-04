768623d7282d1e3960fb4f06fd932ffaa3921491
Michiel van der Velde <michiel@michielvdvelde.nl>
2020-03-04 22:44:38 +0100

    fix: escape double quotes in authorization header params

--- src/util.ts
+++ src/util.ts
@@ -8,7 +8,8 @@ export function array2object (arr: any[]) {
   const obj: { [index: string]: string } = {}
 
   for (let i = 0; i < arr.length; i += 2) {
-    obj[arr[i]] = arr[i + 1]
+    // escape double quotes - https://gist.github.com/getify/3667624
+    obj[arr[i]] = arr[i + 1].replace(/\\([\s\S])|(")/g,"\\$1$2")
   }
 
   return obj
