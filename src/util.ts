312684dae2d18e5ff39d2df5790fae5974d3706c
Michiel van der Velde <michiel@michielvdvelde.nl>
2020-03-05 00:41:26 +0100

    fix: don't escape array value (reverses 768623d)

--- src/util.ts
+++ src/util.ts
@@ -8,8 +8,7 @@ export function array2object (arr: any[]) {
   const obj: { [index: string]: string } = {}
 
   for (let i = 0; i < arr.length; i += 2) {
-    // escape double quotes - https://gist.github.com/getify/3667624
-    obj[arr[i]] = arr[i + 1].replace(/\\([\s\S])|(")/g,"\\$1$2")
+    obj[arr[i]] = arr[i + 1]
   }
 
   return obj
